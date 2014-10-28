ab3d83bc581f42836d7e61d2651602ae3febeeef
ramonmartinl69 <ramonmartinl69@gmail.com>
2014-10-28 17:59:36 +0100

    Added AuthRole & SecurityRoleEnum
    Signed-off-by: ramonmartinl69 <ramonmartinl69@gmail.com>

---
+++ initial-core/src/main/java/org/myorg/initial/roo/core/domain/reference/SecurityRoleEnum.java
@@ -0,0 +1,6 @@
+package org.myorg.initial.roo.core.domain.reference;
+
+public enum SecurityRoleEnum {
+
+    ROLE_USER, ROLE_ANONYMOUS
+}
---
+++ initial-core/src/main/java/org/myorg/initial/roo/core/domain/security/AuthRole.java
@@ -0,0 +1,30 @@
+package org.myorg.initial.roo.core.domain.security;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.serializable.RooSerializable;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+import org.myorg.initial.roo.core.domain.reference.SecurityRoleEnum;
+import javax.validation.constraints.Size;
+import org.springframework.roo.addon.equals.RooEquals;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord(identifierColumn = "id_role", table = "security_role", versionColumn = "OPT_LOCK")
+@RooSerializable
+@RooEquals(excludeFields = { "id", "roleName" })
+public class AuthRole {
+
+    /**
+     */
+    @NotNull
+    @Enumerated
+    private SecurityRoleEnum authority;
+
+    /**
+     */
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String roleName;
+}
